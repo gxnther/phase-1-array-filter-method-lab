4b98961d23b128aac75e643835b811eb3cfbb44b
Gunther <gunt.valentine@gmail.com>
2022-08-17 16:10:28 -0400

    commit tree 1e28d00add96d81b6bce725c4d8372d34bd66ffa

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase())
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(fuzzy => fuzzy.toLowerCase().slice(0, query.length) === query.toLowerCase())
+}
+
+function matchName(drivers, query) {
+    return drivers.filter(hometown => hometown.name === query)
+}
